a583ce0abb9ffad7c5471321b0ad2bd7632e77b6
Vishnu N. Krishnan <navaneeth.vishnu1@gmail.com>
2022-06-01 13:36:08 +0400

    INDEX.JS: Added date and time of receivig request to all endpoints. (Slight modification on how it is displayed).

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ wss.on('connection', function connection(ws){
 
 //API Endpoint for monitor app to authenticate accounts.
 app.post('/app/authenticateAccount', async (request, response) => {
-    console.log(`\n▇▇▇▇ ${Date()} ▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇\n`)
+    console.log(`\n▇▇▇▇ ${Date()} ▇▇▇▇\n`)
     console.log(Date());
     console.log(`New web app authentication request received...\n`);
     const responseForClient = await authenticateWebApp.authenticateWebApp(request.body)
@@ -54,7 +54,7 @@ app.post('/app/authenticateAccount', async (request, response) => {
 
 //API Endpoint to get the details of all vehicles connected to an account as an array of objects
 app.post('/app/getAllVehiclesDetails', async (request, response) => {
-    console.log(`\n▇▇▇▇ ${Date()} ▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇\n`)
+    console.log(`\n▇▇▇▇ ${Date()} ▇▇▇▇\n`)
     console.log(`New request to get all vehicles with details from Account No.: ${request.body.accountId}`)
     const responseForClient = await getAllVehiclesDetailsOnAccount.getAllVehiclesDetailsOnAccount(request.body.accountId)
     //console.log(responseForClient)
@@ -75,7 +75,7 @@ app.post('/app/getAllVehiclesDetails', async (request, response) => {
 //API Endpoint to get the visited Locations on a particular date for a particular vehicleId. Used by component VisitedLocationsList.jsx on the frontend.
 app.post('/app/getVisitedLocations', async (request, response) => {
 
-    console.log(`\n▇▇▇▇ ${Date()} ▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇\n`)
+    console.log(`\n▇▇▇▇ ${Date()} ▇▇▇▇\n`)
     console.log(`New request to collect visited locations...`)
     console.log(`Vehicle ID: ${request.body.vehicleId}`)
     console.log(`Journey Start Date: ${Date(JSON.stringify(request.body.journeyStartDate))}`)
@@ -89,7 +89,7 @@ app.post('/app/getVisitedLocations', async (request, response) => {
 
 //API Endpoint to get the coordinates of all the waypoints travelled by a particular vehicleId from a specified start date to end date. Used by component MapHolder.jsx on the front end app.
 app.post(`/app/getWayPoints`, async (request, response) => {
-    console.log(`\n▇▇▇▇ ${Date()} ▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇\n`)
+    console.log(`\n▇▇▇▇ ${Date()} ▇▇▇▇\n`)
     console.log(`New request to collect Waypoints...`)
     console.log(`Vehicle ID: ${request.body.vehicleId}`)
     console.log(`Journey Start Date: ${Date(JSON.stringify(request.body.journeyStartDate))}`)
@@ -103,7 +103,7 @@ app.post(`/app/getWayPoints`, async (request, response) => {
 
 //API Endpoint to get details for display on VehicleDetailsBar.jsx on the front end.
 app.post('/app/getVehicleDetails', async (request, response) => {
-    console.log(`\n▇▇▇▇ ${Date()} ▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇\n`)
+    console.log(`\n▇▇▇▇ ${Date()} ▇▇▇▇\n`)
     console.log(`New request to collect Vehicle Details for Vehicle ID ${request.body.vehicleId}...`)
     const responseForClient = await getVehicleDetails.getVehicleDetails(request.body.vehicleId)
     console.log(responseForClient)
@@ -114,7 +114,7 @@ app.post('/app/getVehicleDetails', async (request, response) => {
 
 //API Endpoint to get detailed Journey Information. Used by component JourneyInfoDetailed.jsx on the front end.
 app.post('/app/getDetailedJourneyInfo', async (request, response) => {
-    console.log(`\n▇▇▇▇ ${Date()} ▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇\n`)
+    console.log(`\n▇▇▇▇ ${Date()} ▇▇▇▇\n`)
     console.log(`New request to collect Detailed Journey Information...`)
     console.log(`Vehicle ID: ${request.body.vehicleId}`)
     console.log(`Journey Start Date: ${Date(JSON.stringify(request.body.journeyStartDate))}`)
@@ -128,7 +128,7 @@ app.post('/app/getDetailedJourneyInfo', async (request, response) => {
 
 //API Endpoint to link tracker/vehicle with AccountId (trackerId and vehicleId are the same. The name has been interchangeably used.)
 app.post('/app/linkTrackerWithAccount', (request, response) => {
-    console.log(`\n▇▇▇▇ ${Date()} ▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇\n`)
+    console.log(`\n▇▇▇▇ ${Date()} ▇▇▇▇\n`)
     console.log(Date());
     console.log(`New Tracker-Account-Linking request received...\n`);
     linkTrackerWithAccount.linkTrackerWithAccount(
@@ -149,7 +149,7 @@ app.post('/app/linkTrackerWithAccount', (request, response) => {
 
 //API Endpoint for onboard device to authenticate.
 app.post('/api/authenticate', async (request, response) => {
-    console.log(`\n▇▇▇▇ ${Date()} ▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇\n`)
+    console.log(`\n▇▇▇▇ ${Date()} ▇▇▇▇\n`)
     console.log(Date());
     console.log(`New authentication request from vehicleId: ${request.body.vehicleId}`)
     var responseForClient = await authenticateTracker.authenticateTracker(request.body)
@@ -161,7 +161,7 @@ app.post('/api/authenticate', async (request, response) => {
 
 //API Endpoint for onboard device to add waypoint
 app.post('/api/addWayPoints', async (request, response) => {
-    console.log(`\n▇▇▇▇ ${Date()} ▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇\n`)
+    console.log(`\n▇▇▇▇ ${Date()} ▇▇▇▇\n`)
     console.log(Date());
     console.log(`New wayPoints given by vehicleId: ${request.body[0].vehicleId}`)
     console.log(`Number of coords in the request: ${request.body.length}`)
