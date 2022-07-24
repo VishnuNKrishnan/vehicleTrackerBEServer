f3cbd0f13f10fa5cb4e10ca35018d7c2be62574b
Vishnu N. Krishnan <navaneeth.vishnu1@gmail.com>
2022-07-24 13:48:06 +0400

    SMS confirmation for driver removal now working for verified contact numbers

--- module_removeDriverFromVehicleId.js
+++ module_removeDriverFromVehicleId.js
@@ -1,5 +1,6 @@
 const db = require('./module_initializeFirebase')
 const sendDriverRemovedNotification = require('./emailFunctions/sendDriverRemovedNotification_Email')
+const sendDriverRemovedConfirmation_SMS = require('./SMSFunctions/sendDriverRemovedConfirmation_SMS')
 
 async function removeDriverFromVehicle(vehicleId){
 
@@ -13,7 +14,7 @@ async function removeDriverFromVehicle(vehicleId){
     const vehicleIdDataObject = vehicleIdData.data()
 
     //Update the details on the DB:
-    if(db.collection('vehicles').doc(vehicleId).update({displayPictureBase64: '', driverName:'',  driverContact: '', driverEmail:''})){
+    if(db.collection('vehicles').doc(vehicleId).update({displayPictureBase64: '', driverName:'',  driverContact: '', driverEmail:'', driverContactVerified: false})){
         responseData.driverDetailsUpdated = true
         sendDriverRemovedNotification.sendDriverRemoved_Email(
             vehicleIdDataObject.vehicleDescription,
@@ -23,6 +24,7 @@ async function removeDriverFromVehicle(vehicleId){
             vehicleIdDataObject.driverEmail,
             vehicleIdDataObject.displayPictureBase64    
         )
+        vehicleIdDataObject.driverContactVerified ? sendDriverRemovedConfirmation_SMS.sendDriverRemovedConfirmation_SMS(vehicleIdDataObject.driverContact, vehicleIdDataObject.vehicleDescription, vehicleIdDataObject.licensePlate, vehicleIdDataObject.driverName) : null
     }
 
     console.log(`Driver details updation status: ${responseData.driverDetailsUpdated}`)
