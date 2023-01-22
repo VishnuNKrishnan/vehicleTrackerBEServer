2a41c86dc5cc8fc88ec7e4ea8979751d58a78fd5
Vishnu N. Krishnan <navaneeth.vishnu1@gmail.com>
2023-01-22 15:38:46 +0400

    Speeding SMS - SMS content minor formatting

--- SMSFunctions/sendSpeedingAlert_SMS.js
+++ SMSFunctions/sendSpeedingAlert_SMS.js
@@ -4,10 +4,23 @@ function sendSpeedingAlert_SMS(phoneNumber, vehicleDescription, licensePlate, re
 
     const twilio = require('twilio')(sid, authToken)
 
+    const date = new Date(recordedTime);
+    const dateOptions = { timeZone: 'Asia/Dubai', year: 'numeric', month: '2-digit', day: '2-digit' };
+    const formattedDate = date.toLocaleString('en-US', dateOptions);
+    //console.log(dateInDubai); // Output: "25-09-2022"
+
+    const timeOptions = { timeZone: 'Asia/Dubai', hour: '2-digit', minute: '2-digit' };
+    const formattedTime = date.toLocaleTimeString('en-US', timeOptions);
+    //console.log(timeInDubai); // Output: "08:00"
+
+    const recordedTimeFormatted = formattedTime
+    const recordedDateFormatted = formattedDate
+    const legalSpeedLimit = 'NA'
+
     twilio.messages.create({
         from:'+19379091818',
         to: phoneNumber,
-        body:`Hi, your ${vehicleDescription} (${licensePlate}) has exceeded the allowed speed limit.\n\nRecorded Speed: ${recordedSpeed.toFixed()} km/h\nLocation: ${locationMain}, ${locationSub}\nTime: ${recordedTime}\nAssigned to: ${driverName}\n\nPlease abide by and encourage safe driving practices so that everyone reaches home safe and sound.\nThanks,\nknoWhere`
+        body:`Hi, your ${vehicleDescription} (${licensePlate}) has exceeded the allowed speed limit.\n\nRecorded Speed: ${recordedSpeed.toFixed()} km/h\nLocation: ${locationMain}, ${locationSub}\nTime: ${recordedTimeFormatted}, ${recordedDateFormatted}\nAssigned to: ${driverName}\n\nPlease abide by and encourage safe driving practices so that everyone reaches home safe and sound.\nThanks,\nknoWhere`
     }).then(res=>{console.log(`Speeding Alert - SMS Sent!`)}).catch(err=>{log(err)})
 }
 
